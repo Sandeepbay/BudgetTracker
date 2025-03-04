a2115c30e7e09e508c8a5d5b1115ff3038e9e589
Sai Sandeep Acharjyo <sandeepbay2002@gmail.com>
2025-03-04 20:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def homePage():
     return "Welcome to Budget Tracker"
+
+@app.route("/addtransaction")
+def addExpense():
+    return "Here we can add Transacations"
+
+@app.route("/showtransactions")
+def showExpense():
+    return "We can show all transacations here"
 
 if __name__ == "__main__":
     app.run(debug=True)
